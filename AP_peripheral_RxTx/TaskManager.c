fcc2b91b2f4b48228ad0647fd1fe2032e4061e55
Larissa <larissa95.amaral@gmail.com>
2018-12-10 09:36:37 -0200

    Organiza configuração dos semaphores e easy link

--- AP_peripheral_RxTx/TaskManager.c
+++ AP_peripheral_RxTx/TaskManager.c
@@ -67,6 +67,7 @@
 /* Undefine to remove address filter and async mode */
 #define RFEASYLINKRX_ASYNC
 #define RFEASYLINKRX_ADDR_FILTER
+#define RFEASYLINKTX_ASYNC
 
 #define RFEASYLINKEX_TASK_STACK_SIZE 1024
 #define RFEASYLINKEX_TASK_PRIORITY   2
@@ -98,12 +99,14 @@ PIN_Handle pinHandle;
 static uint16_t seqNumber;
 
 #ifdef RFEASYLINKRX_ASYNC
-static Semaphore_Handle doneSemaphore;
+static Semaphore_Handle rxDoneSemaphore;
+#endif
+
+#ifdef RFEASYLINKTX_ASYNC
+static Semaphore_Handle txDoneSemaphore;
 #endif
 
 /***** Function definitions *****/
-static void rfEasyLinkTxFnx();
-static void rfEasyLinkRxFnx();
 
 float getRelativeDistance(uint8_t rssi) {
     //RSSI = -10*n*log10(d) + A
@@ -119,9 +122,6 @@ void rxDoneCb(EasyLink_RxPacket * rxPacket, EasyLink_Status status)
 
             id[data_counter] = rxPacket->payload[0];
             rssi[data_counter] = (-1)*rxPacket->rssi;
-
-            printf("%d\n", rssi[data_counter]);
-            printf("%f\n", getRelativeDistance(rssi[data_counter]));
 
             time_t t = time(NULL);
             struct tm tm = *localtime(&t);
@@ -145,7 +145,7 @@ void rxDoneCb(EasyLink_RxPacket * rxPacket, EasyLink_Status status)
         PIN_setOutputValue(pinHandle, Board_PIN_LED2,!PIN_getOutputValue(Board_PIN_LED2));
     }
 
-    Semaphore_post(doneSemaphore);
+    Semaphore_post(rxDoneSemaphore);
 }
 #endif
 
@@ -168,105 +168,10 @@ void txDoneCb(EasyLink_Status status)
         PIN_setOutputValue(pinHandle, Board_PIN_LED2,!PIN_getOutputValue(Board_PIN_LED2));
     }
 
-    Semaphore_post(doneSemaphore);
+    Semaphore_post(txDoneSemaphore);
 }
 
-static void rfEasyLinkRxFnx()
-{
-#ifndef RFEASYLINKRX_ASYNC
-    EasyLink_RxPacket rxPacket = {0};
-#endif
-
-#ifdef RFEASYLINKRX_ASYNC
-    /* Create a semaphore for Async*/
-    Semaphore_Params params;
-    Error_Block eb;
-
-    /* Init params */
-    Semaphore_Params_init(&params);
-    Error_init(&eb);
-
-    /* Create semaphore instance */
-    doneSemaphore = Semaphore_create(0, &params, &eb);
-    if(doneSemaphore == NULL)
-    {
-        System_abort("Semaphore creation failed");
-    }
-
-#endif //RFEASYLINKRX_ASYNC
-
-	EasyLink_Params easyLink_params;
-    EasyLink_Params_init(&easyLink_params);
-	
-	easyLink_params.ui32ModType = EasyLink_Phy_Custom; 
-
-    /* Initialize EasyLink */
-    if(EasyLink_init(&easyLink_params) != EasyLink_Status_Success)
-    {
-        System_abort("EasyLink_init failed");
-    }
-
-    /*
-     * If you wish to use a frequency other than the default, use
-     * the following API:
-     * EasyLink_setFrequency(868000000);
-     */
-
-#ifdef RFEASYLINKRX_ADDR_FILTER
-	/* 
-     * The address filter is set to match on a single byte (0xAA) but 
-     * EasyLink_enableRxAddrFilter will copy 
-     * EASYLINK_MAX_ADDR_SIZE * EASYLINK_MAX_ADDR_FILTERS
-     * bytes to the address filter bank
-     */
-    uint8_t addrFilter[EASYLINK_MAX_ADDR_SIZE * EASYLINK_MAX_ADDR_FILTERS] = {0xaa};
-    EasyLink_enableRxAddrFilter(addrFilter, 1, 1);
-#endif //RFEASYLINKRX_ADDR_FILTER
-
-    bool sendBuffer = false;
-    while(!sendBuffer) {
-#ifdef RFEASYLINKRX_ASYNC
-        EasyLink_receiveAsync(rxDoneCb, 0);
-
-        if(data_counter >= BUFFER_SIZE) {
-            data_counter = 0;
-            //sendBuffer = true;
-        }
-
-        /* Wait 300ms for Rx */
-        if(Semaphore_pend(doneSemaphore, (300000 / Clock_tickPeriod)) == FALSE)
-        {
-            /* RX timed out abort */
-            if(EasyLink_abort() == EasyLink_Status_Success)
-            {
-               /* Wait for the abort */
-               Semaphore_pend(doneSemaphore, BIOS_WAIT_FOREVER);
-            }
-        }
-#else
-        rxPacket.absTime = 0;
-        EasyLink_Status result = EasyLink_receive(&rxPacket);
-
-        if (result == EasyLink_Status_Success)
-        {
-            /* Toggle LED2 to indicate RX */
-            PIN_setOutputValue(pinHandle, Board_PIN_LED2,!PIN_getOutputValue(Board_PIN_LED2));
-        }
-        else
-        {
-            /* Toggle LED1 and LED2 to indicate error */
-            PIN_setOutputValue(pinHandle, Board_PIN_LED1,!PIN_getOutputValue(Board_PIN_LED1));
-            PIN_setOutputValue(pinHandle, Board_PIN_LED2,!PIN_getOutputValue(Board_PIN_LED2));
-        }
-#endif //RX_ASYNC
-    }
-    rfEasyLinkTxFnx();
-}
-
-static void rfEasyLinkTxFnx()
-{
-    uint8_t txBurstSize = 0;
-    uint32_t absTime;
+void setUpTxSemaphore() {
 
 #ifdef RFEASYLINKTX_ASYNC
     /* Create a semaphore for Async */
@@ -278,17 +183,40 @@ static void rfEasyLinkTxFnx()
     Error_init(&eb);
 
     /* Create semaphore instance */
-    doneSemaphore = Semaphore_create(0, &params, &eb);
-    if(doneSemaphore == NULL)
+    txDoneSemaphore = Semaphore_create(0, &params, &eb);
+    if(txDoneSemaphore == NULL)
     {
         System_abort("Semaphore creation failed");
     }
 
 #endif //TX_ASYNC
+}
 
+void setUpRxSemaphore() {
+
+#ifdef RFEASYLINKRX_ASYNC
+    /* Create a semaphore for Async */
+    Semaphore_Params params;
+    Error_Block eb;
+
+    /* Init params */
+    Semaphore_Params_init(&params);
+    Error_init(&eb);
+
+    /* Create semaphore instance */
+    rxDoneSemaphore = Semaphore_create(0, &params, &eb);
+    if(rxDoneSemaphore == NULL)
+    {
+        System_abort("Semaphore creation failed");
+    }
+
+
+#endif //RX_ASYNC
+}
+
+void setUpEasyLink() {
     EasyLink_Params easyLink_params;
     EasyLink_Params_init(&easyLink_params);
-
     easyLink_params.ui32ModType = EasyLink_Phy_Custom;
 
     /* Initialize EasyLink */
@@ -303,97 +231,156 @@ static void rfEasyLinkTxFnx()
      * EasyLink_setFrequency(868000000);
      */
 
-#if (defined __CC1352P1_LAUNCHXL_BOARD_H__)
-    /* Set output power to 20dBm */
-    EasyLink_Status pwrStatus = EasyLink_setRfPower(20);
-#else
     /* Set output power to 12dBm */
     EasyLink_Status pwrStatus = EasyLink_setRfPower(12);
-#endif
+
     if(pwrStatus != EasyLink_Status_Success)
     {
         // There was a problem setting the transmission power
         while(1);
     }
 
-    uint8_t packet_counter;
-   for(packet_counter = 0; packet_counter < QT_PACKETS; packet_counter++) {
-        EasyLink_TxPacket txPacket =  { {0}, 0, 0, {0} };
+#ifdef RFEASYLINKRX_ADDR_FILTER
+    /*
+     * The address filter is set to match on a single byte (0xAA) but
+     * EasyLink_enableRxAddrFilter will copy
+     * EASYLINK_MAX_ADDR_SIZE * EASYLINK_MAX_ADDR_FILTERS
+     * bytes to the address filter bank
+     */
+    uint8_t addrFilter[EASYLINK_MAX_ADDR_SIZE * EASYLINK_MAX_ADDR_FILTERS] = {0xaa};
+    EasyLink_enableRxAddrFilter(addrFilter, 1, 1);
+#endif //RFEASYLINKRX_ADDR_FILTER
+}
 
-        /* Create packet with buffer on payload */
-        txPacket.payload[0] = (uint8_t)(MY_ID);
+static void taskManagerFnx(UArg a0, UArg a1)
+{
+    setUpTxSemaphore();
+    setUpRxSemaphore();
+    setUpEasyLink();
 
-        uint8_t i = 1;
-        while(i < RFEASYLINKTXPAYLOAD_LENGTH - 3) {
-            time_t t = time(NULL);
-            struct tm tm = *localtime(&t);
-            uint16_t deltaTime = (uint16_t)((( ( ( ( (tm.tm_year - 70)*12 + tm.tm_mon )*30 + (tm.tm_mday - 1) )*24 + tm.tm_hour )*60 + tm.tm_min )*60 + tm.tm_sec) - local_time[data_counter]);
-            uint8_t deltaTimeFirstByte = (uint8_t)(deltaTime/256);
-            uint8_t deltaTimeSecondByte = (uint8_t)(deltaTime - deltaTimeFirstByte*256);
+    while(1) {
+        //Start RX Task
+    #ifndef RFEASYLINKRX_ASYNC
+        EasyLink_RxPacket rxPacket = {0};
+    #endif
 
-            txPacket.payload[i++] = id[data_counter];
-            txPacket.payload[i++] = rssi[data_counter];
-            txPacket.payload[i++] = deltaTimeFirstByte;
-            txPacket.payload[i++] = deltaTimeSecondByte;
+        data_counter = 0;
+        while(data_counter < BUFFER_SIZE) {
+    #ifdef RFEASYLINKRX_ASYNC
+            EasyLink_receiveAsync(rxDoneCb, 0);
 
-            data_counter++;
-        }
-
-        txPacket.len = RFEASYLINKTXPAYLOAD_LENGTH;
-        txPacket.dstAddr[0] = 0xbb;
-
-        /* Add a Tx delay for > 500ms, so that the abort kicks in and brakes the burst */
-        if(EasyLink_getAbsTime(&absTime) != EasyLink_Status_Success)
-        {
-            // Problem getting absolute time
-        }
-        if(txBurstSize++ >= RFEASYLINKTX_BURST_SIZE)
-        {
-          /* Set Tx absolute time to current time + 1s */
-          txPacket.absTime = absTime + EasyLink_ms_To_RadioTime(1000);
-          txBurstSize = 0;
-        }
-        /* Else set the next packet in burst to Tx in 100ms */
-        else
-        {
-          /* Set Tx absolute time to current time + 100ms */
-          txPacket.absTime = absTime + EasyLink_ms_To_RadioTime(100);
-        }
-
-#ifdef RFEASYLINKTX_ASYNC
-        EasyLink_transmitAsync(&txPacket, txDoneCb);
-        /* Wait 300ms for Tx to complete */
-        if(Semaphore_pend(doneSemaphore, (300000 / Clock_tickPeriod)) == FALSE)
-        {
-            /* TX timed out, abort */
-            if(EasyLink_abort() == EasyLink_Status_Success)
+            /* Wait 300ms for Rx */
+            if(Semaphore_pend(rxDoneSemaphore, (300000 / Clock_tickPeriod)) == FALSE)
             {
-                /*
-                 * Abort will cause the txDoneCb to be called and the doneSemaphore
-                 * to be released, so we must consume the doneSemaphore
-                 */
-               Semaphore_pend(doneSemaphore, BIOS_WAIT_FOREVER);
+                /* RX timed out abort */
+                if(EasyLink_abort() == EasyLink_Status_Success)
+                {
+                   /* Wait for the abort */
+                   Semaphore_pend(rxDoneSemaphore, BIOS_WAIT_FOREVER);
+                }
             }
-        }
-#else
-        EasyLink_Status result = EasyLink_transmit(&txPacket);
+    #else
+            rxPacket.absTime = 0;
+            EasyLink_Status result = EasyLink_receive(&rxPacket);
 
-        if (result == EasyLink_Status_Success)
-        {
-            /* Toggle LED1 to indicate TX */
-            PIN_setOutputValue(pinHandle, Board_PIN_LED1,!PIN_getOutputValue(Board_PIN_LED1));
+            if (result == EasyLink_Status_Success)
+            {
+                /* Toggle LED2 to indicate RX */
+                PIN_setOutputValue(pinHandle, Board_PIN_LED2,!PIN_getOutputValue(Board_PIN_LED2));
+            }
+            else
+            {
+                /* Toggle LED1 and LED2 to indicate error */
+                PIN_setOutputValue(pinHandle, Board_PIN_LED1,!PIN_getOutputValue(Board_PIN_LED1));
+                PIN_setOutputValue(pinHandle, Board_PIN_LED2,!PIN_getOutputValue(Board_PIN_LED2));
+            }
+    #endif //RX_ASYNC
         }
-        else
-        {
-            /* Toggle LED1 and LED2 to indicate error */
-            PIN_setOutputValue(pinHandle, Board_PIN_LED1,!PIN_getOutputValue(Board_PIN_LED1));
-            PIN_setOutputValue(pinHandle, Board_PIN_LED2,!PIN_getOutputValue(Board_PIN_LED2));
+
+        //Stopping RX to send buffer
+        data_counter = 0;
+
+        //Entering TX
+        uint8_t txBurstSize = 0;
+        uint32_t absTime;
+
+        //Starting to send packets
+        uint8_t packet_counter;
+       for(packet_counter = 0; packet_counter < QT_PACKETS; packet_counter++) {
+            EasyLink_TxPacket txPacket =  { {0}, 0, 0, {0} };
+
+            /* Create packet with buffer on payload */
+            txPacket.payload[0] = (uint8_t)(MY_ID);
+
+            uint8_t i = 1;
+            while(i < RFEASYLINKTXPAYLOAD_LENGTH - 3) {
+                time_t t = time(NULL);
+                struct tm tm = *localtime(&t);
+                uint16_t deltaTime = (uint16_t)((( ( ( ( (tm.tm_year - 70)*12 + tm.tm_mon )*30 + (tm.tm_mday - 1) )*24 + tm.tm_hour )*60 + tm.tm_min )*60 + tm.tm_sec) - local_time[data_counter]);
+                uint8_t deltaTimeFirstByte = (uint8_t)(deltaTime/256);
+                uint8_t deltaTimeSecondByte = (uint8_t)(deltaTime - deltaTimeFirstByte*256);
+
+                txPacket.payload[i++] = id[data_counter];
+                txPacket.payload[i++] = rssi[data_counter];
+                txPacket.payload[i++] = deltaTimeFirstByte;
+                txPacket.payload[i++] = deltaTimeSecondByte;
+
+                data_counter++;
+            }
+
+            txPacket.len = RFEASYLINKTXPAYLOAD_LENGTH;
+            txPacket.dstAddr[0] = 0xbb;
+
+            /* Add a Tx delay for > 500ms, so that the abort kicks in and brakes the burst */
+            if(EasyLink_getAbsTime(&absTime) != EasyLink_Status_Success)
+            {
+                // Problem getting absolute time
+            }
+            if(txBurstSize++ >= RFEASYLINKTX_BURST_SIZE)
+            {
+              /* Set Tx absolute time to current time + 1s */
+              txPacket.absTime = absTime + EasyLink_ms_To_RadioTime(1000);
+              txBurstSize = 0;
+            }
+            /* Else set the next packet in burst to Tx in 100ms */
+            else
+            {
+              /* Set Tx absolute time to current time + 100ms */
+              txPacket.absTime = absTime + EasyLink_ms_To_RadioTime(100);
+            }
+
+    #ifdef RFEASYLINKTX_ASYNC
+            EasyLink_transmitAsync(&txPacket, txDoneCb);
+            /* Wait 300ms for Tx to complete */
+            if(Semaphore_pend(txDoneSemaphore, (300000 / Clock_tickPeriod)) == FALSE)
+            {
+                /* TX timed out, abort */
+                if(EasyLink_abort() == EasyLink_Status_Success)
+                {
+                    /*
+                     * Abort will cause the txDoneCb to be called and the doneSemaphore
+                     * to be released, so we must consume the doneSemaphore
+                     */
+                   Semaphore_pend(txDoneSemaphore, BIOS_WAIT_FOREVER);
+                }
+            }
+    #else
+            EasyLink_Status result = EasyLink_transmit(&txPacket);
+
+            if (result == EasyLink_Status_Success)
+            {
+                /* Toggle LED1 to indicate TX */
+                PIN_setOutputValue(pinHandle, Board_PIN_LED1,!PIN_getOutputValue(Board_PIN_LED1));
+            }
+            else
+            {
+                /* Toggle LED1 and LED2 to indicate error */
+                PIN_setOutputValue(pinHandle, Board_PIN_LED1,!PIN_getOutputValue(Board_PIN_LED1));
+                PIN_setOutputValue(pinHandle, Board_PIN_LED2,!PIN_getOutputValue(Board_PIN_LED2));
+            }
+    #endif //RFEASYLINKTX_ASYNC
         }
-#endif //RFEASYLINKTX_ASYNC
     }
-
-   data_counter = 0;
-    rfEasyLinkRxFnx();
 }
 
 void TaskManager_init(PIN_Handle ledPinHandle) {
@@ -405,5 +392,5 @@ void TaskManager_init(PIN_Handle ledPinHandle) {
     taskParams.stack = &taskStack;
     taskParams.arg0 = (UInt)1000000;
 
-    Task_construct(&task, rfEasyLinkRxFnx, &taskParams, NULL);
+    Task_construct(&task, taskManagerFnx, &taskParams, NULL);
 }
